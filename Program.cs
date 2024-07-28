367fb79a532d4bf9588aa1288d9f7a18feb1d41b
Manoelrev <jmanoel890@gmail.com>
2024-07-28 20:39:35 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+					
+public class Program
+{	
+	static int[] MenorDistanciaArray(int[] arrayUm, int[] arraysDois)
+	{
+	  int[] resultado = [9999,0,0];
+
+	  int[] calculoDistancia(int valorUm, int ValorDois)
+	  {
+		var cal = Math.Abs((valorUm < 0 || ValorDois < 0) ? Math.Abs(valorUm) + Math.Abs(ValorDois) : valorUm - ValorDois);
+		return  resultado = (resultado[0] > cal) ? [cal, valorUm, ValorDois] : resultado;
+	  }
+
+	  for (int x = 0; x < arrayUm.Length; x++)
+	  {
+		for(int y = 0; y < arraysDois.Length; y++) 
+		{
+		  // Compara Duas Array diferentes
+		  calculoDistancia(arrayUm[x],arraysDois[y]);
+		  if(x != y)
+		  {
+			// Compara a Array UM
+			calculoDistancia(arrayUm[x],arrayUm[y]);
+			// Compara a Array Dois
+			calculoDistancia(arraysDois[x],arraysDois[y]);
+		  }
+		}
+	  }
+		return [resultado[1], resultado[2]];
+	}
+
+	
+	public static void Main()
+	{
+		int[] result = MenorDistanciaArray([-1, 5], [26, 6]);
+		Console.WriteLine(result[0]+", "+result[1]);
+	}
+}
